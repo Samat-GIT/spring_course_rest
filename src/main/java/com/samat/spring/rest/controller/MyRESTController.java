3d9c1d912cb1e35cd18897dbb51f4adbc7e1f1b0
Samat-GIT <seitmuratov.samat@bk.ru>
2022-11-09 20:42:47 +0600

    Initial commit

---
+++ src/main/java/com/samat/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,42 @@
+package com.samat.spring.rest.controller;
+
+
+import com.samat.spring.rest.entity.Employee;
+import com.samat.spring.rest.exeption_handling.EmployeeIncorrectData;
+import com.samat.spring.rest.exeption_handling.NoSuchEmployeeException;
+import com.samat.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee>showAllEmployees(){
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee geyEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+
+        if(employee == null){
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                   id + " in DataBase");
+        }
+
+        return employee;
+    }
+
+
+
+}
